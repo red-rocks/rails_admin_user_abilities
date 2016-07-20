d93465f514a2a2129850e8b71cccbf71cc9e939e
Alexander Kiseliev <i43ack@gmail.com>
2016-07-20 11:00:57 +0300

    fix for undefined classes

--- lib/rails_admin_user_abilities/models/user_ability.rb
+++ lib/rails_admin_user_abilities/models/user_ability.rb
@@ -8,14 +8,14 @@ module RailsAdminUserAbilities
 
         def to_cancancan(ability_object)
           abilities.each_pair do |model_name, rules|
-            _model = model_name.constantize
+            _model = model_name.constantize if defined?(model_name)
             rules.each_pair do |act, meth|
               ability_object.send(meth, act.to_sym, _model)
             end
           end
 
           accesses.each_pair do |model_name, ids_rules|
-            _model = model_name.constantize
+            _model = model_name.constantize if defined?(model_name)
             ids_rules.each_pair do |obj_id, rules|
               rules.each_pair do |act, meth|
                 ability_object.send(meth, act.to_sym, _model, {id: BSON::ObjectId.from_string(obj_id)})
